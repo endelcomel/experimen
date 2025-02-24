1507a7767617609204d3d1f6fc33d40742daf105
endelcomel <66982299+endelcomel@users.noreply.github.com>
2025-02-24 13:19:42 +0700

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { createDbWorker } from "sql.js-httpvfs";
+
+const workerUrl = new URL(
+  "sql.js-httpvfs/dist/sqlite.worker.js",
+  import.meta.url
+);
+const wasmUrl = new URL("sql.js-httpvfs/dist/sql-wasm.wasm", import.meta.url);
+
+async function load() {
+  const worker = await createDbWorker(
+    [
+      {
+        from: "inline",
+        config: {
+          serverMode: "full",
+          url: "/data/db_1.db",
+          requestChunkSize: 4096,
+        },
+      },
+    ],
+    workerUrl.toString(),
+    wasmUrl.toString()
+  );
+
+  const result = await worker.db.query(`select * from questions`);
+
+  document.body.textContent = JSON.stringify(result);
+}
+
+load();
