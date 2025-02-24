a8e9726a517be8b61211dc6d61d9f41ff6bc7375
endelcomel <66982299+endelcomel@users.noreply.github.com>
2025-02-24 13:39:06 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,31 @@
+import "./styles.css";
 import { createDbWorker } from "sql.js-httpvfs";
 
-const workerUrl = new URL(
-  "sql.js-httpvfs/dist/sqlite.worker.js",
-  import.meta.url
-);
-const wasmUrl = new URL("sql.js-httpvfs/dist/sql-wasm.wasm", import.meta.url);
+const workerUrl = "/src/sqlite.worker.js";
+const wasmUrl = "/assets/sql-wasm.wasm";
 
 async function load() {
+  console.log("start load");
   const worker = await createDbWorker(
     [
       {
         from: "inline",
         config: {
           serverMode: "full",
-          url: "/data/db_1.db",
-          requestChunkSize: 4096,
-        },
-      },
+          url: "example.sqlite3",
+          requestChunkSize: 4096
+        }
+      }
     ],
-    workerUrl.toString(),
-    wasmUrl.toString()
+    workerUrl,
+    wasmUrl
   );
 
-  const result = await worker.db.query(`select * from questions`);
+  console.log("before awaitworkerdb");
+  const result = await worker.db.query(`select * from mytable`);
 
-  document.body.textContent = JSON.stringify(result);
+  document.getElementById("app").innerHTML = JSON.stringify(result);
 }
 
+console.log(load);
 load();
